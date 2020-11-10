d4334605b51bd40ea3beba6a5a277e6dbefd0da6
shinya-jp <shinya0733@gmail.com>
2020-11-10 21:51:10 +0900

    繰り返し処理

---
+++ sample06.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+$i = 1;
+print($i. "\n");
+
+while ($i <= 36) {    #while (繰り返し条件) {
+	print($i. "\n");  #繰り返したい処理
+	$i = $i + 1;      #更新処理
+}
+
+?>
+</pre>
+</main>
+</body>
+</html>
