a8d7946b4e4f223294291c66cb51cb519f062526
shinya-jp <shinya0733@gmail.com>
2020-11-25 21:32:57 +0900

    少数を整数に切り上げ・下げ

---
+++ sample11.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<h2>Practice</h2>
+<pre>
+3000円のものから、100円引きした場合は、<?php print(floor(100 / 3000 * 100)); ?>%引きです
+<!-- floor　少数を切り捨てる -->
+*その他の計算
+切り上げ(ceil) <?php print(ceil(100 / 3000 * 100)); ?>
+
+四捨五入(round) <?php print(round(100 / 3000 * 100, 1)); ?>
+</pre>
+</pre>
+</main>
+</body>
+</html>
