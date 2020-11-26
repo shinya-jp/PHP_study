57d8605c956ce34a2007f33359302b9dee8470ad
shinya-jp <shinya0733@gmail.com>
2020-11-26 17:58:44 +0900

    jsonの情報を読み込む

---
+++ sample16.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+// ファイルの内容を読み込む
+$file = file_get_contents('https://h2o-space.com/feed/json/');
+// json形式に変える
+$json = json_decode($file);
+
+foreach ($json->items as $item):
+?>
+・<a href="<?php print($item->url); ?>"><?php print($item->title); ?></a>
+<?php
+endforeach;
+?>
+</pre>
+</main>
+</body>
+</html>
