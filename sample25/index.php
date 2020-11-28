8656586d36e53cbdf0d75f86c83a6aafa16054b0
shinya-jp <shinya0733@gmail.com>
2020-11-28 21:13:42 +0900

    セッションに値を保存する

---
+++ sample25/index.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+	$_SESSION['session_message'] = '値をセッションに保存しました';
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+	<a href="page03.php">page03へ</a>
+</pre>
+</main>
+</body>
+</html>
