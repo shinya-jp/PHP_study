6bdbfd9a01a0470dc04558decec215a5f3a5e014
shinya-jp <shinya0733@gmail.com>
2020-11-28 21:01:29 +0900

    Cookieに値を保存する

---
+++ sample24/index.php
@@ -0,0 +1,30 @@
+<?php
+$value = '変数に保存した値です';
+setcookie('save_message', 'cookieに保存した値です', time() + 60 * 60 * 24 *14); #１４日間保存される
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+	<a href="page02.php">page02へ</a>
+</pre>
+</main>
+</body>
+</html>
