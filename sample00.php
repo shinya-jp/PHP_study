6e45ad9287fe24f2d4a1366ca0a43d82f7958a3c
shinya-jp <shinya0733@gmail.com>
2020-12-03 22:01:24 +0900

    memo

--- sample00.php
+++ sample00.php
@@ -21,6 +21,29 @@
 <?php
 
 echo "Mysqlの学習はじめました。"
+/*memo
+
+SQL文は基本的には大文字
+データベースの中身を見たいとき
+ 　SELECT  *      FROM 変数;
+訳　見る　 すべて  どこから　変数を
+
+データの追加
+INSERT INTO items SET id=数値, name = “入力値”
+
+指定したIDのデータを編集する方法
+UPDATE my_items SET price = 180 WHERE id = 1;
+更新する　どのデータの　変更したい箇所 　変更したい場所
+WHERE以降（指定しないと）すべてが変更されてしまう
+
+同じIDや名前にしたくない場合
+primaryキーに設定する
+IDは必ずprimaryキーに設定したほうが良い
+
+AUTO_INCREMENT 自動でIDを順番に採番する
+12 4 など3番めが削除されている場合は3に割り振りされるのではなく、5番になる
+
+*/
 
 ?>
 </pre>
